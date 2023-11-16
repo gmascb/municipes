e50d32806c6e6e8df45ddd3e8d201a12291a4da5
Guilherme Barbosa <g.mascarenhas@nofraud.com>
2023-11-16 15:46:56 -0300

    first

--- app/mailers/municipes_mailer.rb
+++ app/mailers/municipes_mailer.rb
@@ -2,7 +2,6 @@ class MunicipesMailer < ApplicationMailer
 
   def novo_municipe(municipe)
     @municipe = municipe
-    # @url = endereco_new_municipe_url(@municipe.id)
     mail(to: @municipe.email, subject: I18n.t('welcome'))
   end
 
